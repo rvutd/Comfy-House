278ded767552428542151247fcb53239dc09f787
Ravindra Purohit <ravindrapurohit2011@gmail.com>
2021-03-02 22:53:42 +0530

    'organised-stuff'

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
+// Variables -
 const navIcon = document.querySelector('.nav-icon');
 const closeMenu = document.querySelector('.close-menu');
 const menu = document.querySelector('.menu');
 const menuOverlay = document.querySelector('.menu-overlay');
 
-
+// Opens Menu Bar
 navIcon.addEventListener('click', (e)=>{
     menu.classList.add('showMenu');
     menu.classList.add('transparentMenu');
 });
 
+// Closees Menu Bar
 closeMenu.addEventListener('click', (e)=>{
     menu.classList.remove('showMenu');
     menu.classList.remove('transparentMenu');
